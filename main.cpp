33b28e5484169d752a664862b16a379796377578
zhytkovd <zhytkovd@berea.edu>
2025-01-15 11:27:05 -0500

    Changed a few lines to create a merge conflict

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
     Edits made by:
-    TODO: <Add name of student 1 Here> -- to be added by student 1
+    Zhytkov Denys
     TODO: <Add name of student 2 Here> -- to be added by student 2
     TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
  */
@@ -22,8 +22,9 @@ int main() {
     cin >> ducknum;
 
     for (int i = 0; i < ducknum; i++) { 
-        cout << "                __     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
-        cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
+        cout << "                __     " << endl;
+        cout << "              aw  __     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
+        cout << "              <(o )_dw__-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
         cout << "               ( .__> /" << endl; // student 3(2) may change this line
         cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
     }
